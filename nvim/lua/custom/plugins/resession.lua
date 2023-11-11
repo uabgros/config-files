53d6586169aa605106d6ee8e10f7c520a12c39f2
uabgros <50678016+uabgros@users.noreply.github.com>
2023-11-11 22:58:19 +0100

    indent-blackline disabled

--- nvim/lua/custom/plugins/resession.lua
+++ nvim/lua/custom/plugins/resession.lua
@@ -98,18 +98,14 @@ return
       },
     },
 
-    {
-      -- Add indentation guides even on blank lines
-      'lukas-reineke/indent-blankline.nvim',
-      -- Enable `lukas-reineke/indent-blankline.nvim`
-      -- See `:help indent_blankline.txt`
-      opts = {
-        char = '┊',
-        show_trailing_blankline_indent = false,
-        show_current_context = true,
-        -- show_current_context_start = true,
-      },
-    },
+    -- {
+    --   "lukas-reineke/indent-blankline.nvim",
+    --   main = "ibl",
+    --   -- opts = {},
+    --   config = function()
+    --   require("ibl").setup()
+    -- end,
+    -- },
 
     {
       "folke/flash.nvim",
